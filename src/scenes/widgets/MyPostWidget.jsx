6a1db1e296cc3917da37758ffa785fc8e0872d50
Müşerref Selçuk Özdemir <s_05_60@hotmail.com>
2023-01-15 16:46:11 +0300

    client_server

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -60,7 +60,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`http://localhost:3001/posts`, {
+    const response = await fetch(`https://my-smu-apim.onrender.com/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
